87e6d97b2bdf119c5d166bc019a6a95e3a6f837d
RabadonDex <lovallenunez@gmail.com>
2025-08-25 02:40:35 -0400

    ejerrcicio2

---
+++ R codes/Practica 3: Tipos de variable.R
@@ -0,0 +1,14 @@
+######################################
+#PRÁCTICA 3: TIPOS DE VARIABLES
+######################################
+
+print("¿Qué tipo de variable es promedio? Usa class(promedio)")
+
+promedio = 3.95;
+class(promedio);
+
+nombre = "Rabadon";
+class(nombre);
+promedio;
+
+print("Al sumar la variable nombre y promedio, no se podran sumar ya que \n no se puede concatenar strings y ints ");
